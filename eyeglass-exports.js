880a3570fa6d1dc0a29a15d50cfc556951a9bf5a
strarsis <strarsis@gmail.com>
2015-09-13 19:40:51 +0200

    Initial commit.

---
+++ eyeglass-exports.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var path = require('path'),
+      fs = require('fs');
+
+module.exports = function(eyeglass, sass) {
+  return {
+    sassDir: path.join(__dirname, 'sass'),
+    // TODO: eyeglass assets?
+    functions: {
+      'file-text($path)': function(path, encoding: \'utfs8\', done) {
+        fs.readFile(path.getValue(), encoding, function (err,data) {
+          if (err) return console.log(err); // TODO: sass error object?
+          done(sass.types.String(data));
+        });
+      }
+    }
+  };
+};
