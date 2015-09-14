2f20a173dbcd88c497b768400db34b0e8d4b42a6
strarsis <strarsis@gmail.com>
2015-09-14 14:48:55 +0200

    Fix encoding default string.

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -8,7 +8,7 @@ module.exports = function(eyeglass, sass) {
     sassDir: path.join(__dirname, 'sass'),
     // TODO: eyeglass assets?
     functions: {
-      'file-text($path)': function(path, encoding: \'utfs8\', done) {
+      'file-text($path)': function(path, encoding: \'utf8\', done) {
         fs.readFile(path.getValue(), encoding, function (err,data) {
           if (err) return console.log(err); // TODO: sass error object?
           done(sass.types.String(data));
