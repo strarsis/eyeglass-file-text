a054203d176bbbc349f88a3bb30e1da3e7b91da1
strarsis <strarsis@gmail.com>
2015-09-15 18:40:31 +0200

    Fix syntax errors. Fix encoding argument usage.

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -8,8 +8,8 @@ module.exports = function(eyeglass, sass) {
     sassDir: path.join(__dirname, 'sass'),
     // TODO: eyeglass assets?
     functions: {
-      '_file-text($path, $encoding: \'utf8\')': function(path, encoding: \'utf8\', done) {
-        fs.readFile(path.getValue(), encoding, function (err, data) {
+      '_file-text($path, $encoding: \'utf8\')': function(path, encoding, done) {
+        fs.readFile(path.getValue(), encoding.getValue(), function (err, data) {
           if (err) throw new Error("File read error: \'" + err.toString() + "\'.");
           done(sass.types.String(data));
         });
